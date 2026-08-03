153afb6f0ca415e4c4a6c0661a72637347622d83
S <292445316+S202677@users.noreply.github.com>
2026-08-03 16:21:53 +0900

    commit

--- routes/dog.js
+++ routes/dog.js
@@ -1,10 +1,15 @@
+const express =require('express');
+const router = express.Router();
 const request = require('request');
+
+router.get('/', function(req, res){
 request('https://dog.ceo/api/breeds/image/random', function(error, response, body) {
 if(!error&& response.statusCode== 200) {
 const data = JSON.parse(body);
-console.log(body);
 const dogImageUrl = data.message;
-console.log(dogImageUrl);
+res.send('<img src="'+ dogImageUrl + '">');
 }
 });
+});
 
+module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const url =process.env.MONGODB_URI;
+const uri =process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 
